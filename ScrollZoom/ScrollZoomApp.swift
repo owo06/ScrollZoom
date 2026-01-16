d5430409ef246bfb4501691877946dded2f9c423
owo06 <itoka1991.dev@gmail.com>
2026-01-17 02:22:40 +0900

    Initial Commit

---
+++ ScrollZoom/ScrollZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollZoomApp.swift
+//  ScrollZoom
+//
+//  Created by Mine Yoshida on 2026/01/17.
+//
+
+import SwiftUI
+
+@main
+struct ScrollZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
